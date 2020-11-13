72be68f19c8084be12b98db7ae2b88dfe7af30c0
Danilo Lira <danilo.lira01@gmail.com>
2020-11-13 10:45:45 -0300

    Fixes project path

--- Tear/Tear/Controllers/GameViewController.swift
+++ Tear/Tear/Controllers/GameViewController.swift
@@ -15,7 +15,7 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "./art.scnassets/ship.scn")!
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
